3e7542be0f38dfe699624f23a73d50969da6f320
Saeed Afzal <saeed.afzal@protonmail.com>
2024-09-22 00:50:00 +0100

    Complete controls and playlist looping

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import locale
 import os
 import mpv
+import qdarktheme
 
 from pathlib import Path
 from PyQt6.QtWidgets import *
@@ -18,13 +19,14 @@ class Seeker(QSlider):
 
     def mousePressEvent(self, event):
         if event.button() == Qt.MouseButton.LeftButton:
-            position = event.position().x() / self.width()
-            new_value = self.minimum() + position * (self.maximum() - self.minimum())
-            self.setValue(int(new_value))
+            if self.player.playback_time is not None:
+                position = event.position().x() / self.width()
+                new_value = self.minimum() + position * (self.maximum() - self.minimum())
+                self.setValue(int(new_value))
 
-            duration = self.player.duration or 1
-            seekTime = (new_value / 1000) * duration
-            self.player.seek(seekTime, reference="absolute")
+                duration = self.player.duration or 1
+                seekTime = (new_value / 1000) * duration
+                self.player.seek(seekTime, reference="absolute")
 
         super().mousePressEvent(event) 
 
@@ -60,7 +62,10 @@ class Dashboard(QWidget):
         playButton.clicked.connect(self.playAudio)
 
         pauseButton = QPushButton("Pause")
+        pauseButton.clicked.connect(self.pauseAudio)
+
         stopButton = QPushButton("Stop")
+        stopButton.clicked.connect(self.stopAudio)
 
         conLayout = QHBoxLayout()
         conLayout.addWidget(playButton)
@@ -86,6 +91,10 @@ class Dashboard(QWidget):
 
         self.setLayout(layout)
 
+        # -- Events
+        self.player.observe_property("time-pos", self.onTrackEnd)
+        # -- Events
+
     def loadMusicDirectory(self):
         self.path = f"{Path.home()}/Music"
         if not Path(self.path).is_dir():
@@ -105,6 +114,10 @@ class Dashboard(QWidget):
             self.playlist.addItem(os.path.basename(name))
 
     def playAudio(self):
+        if self.player.pause:
+            self.player.pause = False
+            return
+
         item = self.playlist.currentItem()
         if item is None:
             return
@@ -117,11 +130,29 @@ class Dashboard(QWidget):
         print("Playing...")
         self.player.play(absolute)
 
+    def pauseAudio(self):
+        # if not self.player.core_idle:
+        self.player.pause = not self.player.pause
+
+    def stopAudio(self):
+        self.player.stop()
+        self.player.pause = False
+        self.seekSlider.setValue(0)
+
     def updateSlider(self):
         if self.player.time_pos:
             duration = self.player.duration or 0
             if duration > 0:
                 self.seekSlider.setValue(int((self.player.time_pos / duration) * 1000))
+
+    def onTrackEnd(self, name, value):
+        if value is None:
+            print("Track ended. Looping...")
+            index = self.playlist.currentRow() + 1
+            if index >= self.playlist.count():
+                index = 0
+            self.playlist.setCurrentRow(index)
+            self.playAudio()
 
 
 class Window(QMainWindow):
@@ -139,6 +170,7 @@ if __name__ == "__main__":
     player = mpv.MPV(ao="pulse", log_handler=print)
 
     app = QApplication(sys.argv)
+    app.setStyleSheet(qdarktheme.load_stylesheet())
 
     window = Window(player)
     window.show()
